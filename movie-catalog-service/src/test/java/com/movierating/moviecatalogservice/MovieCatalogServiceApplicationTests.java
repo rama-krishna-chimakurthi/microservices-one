d9140cd7c6668f2f32b9f05efc81eac11ad104c3
rama-krishna-chimakurthi <ramakrishnachimakurthy@gmail.com>
2023-08-26 10:47:02 +0530

    initial commit

---
+++ movie-catalog-service/src/test/java/com/movierating/moviecatalogservice/MovieCatalogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movierating.moviecatalogservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieCatalogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
